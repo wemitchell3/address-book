50af7ac45e6327da6551969c6bbf356ad3205c47
William Mitchell <wemitchell3@gmail.com>
2019-03-06 15:43:05 -0600

    Upload finished address-book practice.

---
+++ address.js
@@ -0,0 +1,21 @@
+const container = document.querySelector("#thingLocation")
+
+document.querySelector("#saveEntry").addEventListener("click", event => {
+    /*
+        Collect the user put by selecting the input fields, one
+        at a time, and accessing the `value` property
+    */
+    const thing = document.querySelector("#thing").value
+    const location = document.querySelector("#location").value
+
+ 
+    console.log("Thing", thing.value)
+    console.log("location", location.value)
+    // Once you have collected all the values, update the DOM
+    // with some HTML
+    container.innerHTML += `
+        <section>
+            <h1>"I can purhase${thing} at ${location}."</h1>            
+        </section>
+    `
+})
